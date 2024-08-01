774061520d2a0d9f5895ec4df0da0f742fbb1a6f
topv8899 <vanto8899@gmail.com>
2024-08-01 15:38:05 +0700

    backend completed

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8888;
+
+app.use(express.json()); // to parse the incoming requests with json payload (from req.body)
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
